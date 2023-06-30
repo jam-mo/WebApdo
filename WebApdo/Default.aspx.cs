869bca591a41b5e8f25ce43431386d2b545ba421
Jamie <jamiemolseed@hotmail.co.uk>
2023-06-30 11:12:15 +0100

    Add project files.

---
+++ WebApdo/Default.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace WebApdo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Response.Write("<center><h1>Read data from a database</h1></center><hr/>");
+                Response.Write("<br/>");
+                string s = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+                SqlConnection con = new SqlConnection(s);
+                string sqlString = "select * from customers";
+                SqlCommand cmd = new SqlCommand(sqlString, con);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+                dr.Close();
+                string sqlStringDropDownList = "select Country from customers";
+                SqlCommand cmd2 = new SqlCommand(sqlStringDropDownList, con);
+                SqlDataReader dr2 = cmd2.ExecuteReader();
+                while (dr2.Read() == true)
+                {
+                    DropDownList1.Items.Add(new ListItem(dr2["Country"].ToString(), dr2["Country"].ToString()));
+                }
+                dr2.Close();
+                con.Close();
+
+            }
+        }
+
+     
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            Response.Write("<center><h1>Read data from a database</h1></center><hr/>");
+            Response.Write("<br/>");
+            String txtvalue = TextBox1.Text;
+            string s = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+            SqlConnection con = new SqlConnection(s);
+            string sqlString = "select * from customers where Country=@Country";
+            SqlCommand cmd = new SqlCommand(sqlString, con);
+            // to prevent sql injection
+            cmd.Parameters.AddWithValue("@Country", txtvalue);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            GridView1.DataSource = dr;
+            GridView1.DataBind();
+            dr.Close();
+            con.Close();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                Response.Write("<br/>");
+                String txtValue = DropDownList1.SelectedValue.ToString();
+                string s = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+                SqlConnection con = new SqlConnection(s);
+                string sqlString = "select * from customers where Country=@Country";
+                SqlCommand cmd = new SqlCommand(sqlString, con);
+                cmd.Parameters.AddWithValue("@Country", txtValue);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+                dr.Close();
+                con.Close();
+            }
+
+        }
+    }
+}
---
+++ WebApdo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebApdo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApdo")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55aa278c-5df7-4fea-8b84-ec646cf74c58")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
